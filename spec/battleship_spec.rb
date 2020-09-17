61c5c919da85ca4667452c016b0c4060417e0ee4
ChrisReilly024 <creilly024@gmail.com>
2020-09-16 21:51:06 -0400

    completed all tests for :: checker

--- spec/battleship_spec.rb
+++ spec/battleship_spec.rb
@@ -33,6 +33,27 @@ describe "battleship checker" do
             expect(checker([[0,0,0,0,1,1,0,0,0,0],[0,0,0,0,0,1,0,0,0,0]])).to eq(false)
             expect(checker([[0,0,0,0,0,1,0,0,0,0],[0,0,0,0,0,1,1,0,0,0]])).to eq(false)
         end
-
+        it "should return true if i == 0 and a vertic ship borders no other ship to either side" do
+            expect(checker([[0,0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+            expect(checker([[0,0,0,0,0,1,0,0,0,0],[0,0,0,0,0,1,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+        end
+        it "should return true if j == 0 and a vertic ship borders no other ship to the right" do
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+        end
+        it "should return false if j == 0 and a vertic ship borders a ship to the right of it" do
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(false)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(false)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0]])).to eq(false)
+        end
+        it "should return true if j == 0 and a horz ship borders no other ship below or above it" do
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(true)
+        end
+        it "should return false if j == 0 and a horz ship borders a ship below or above it" do
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(false)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])).to eq(false)
+            expect(checker([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0,0]])).to eq(false)
+        end
     end    
 end
